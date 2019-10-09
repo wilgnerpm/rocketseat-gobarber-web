6a9c916a66b7fa32426de24317bac1f8f7b5f5d2
Wilgner Pinheiro <wilgnerp@hotmail.com>
2019-10-09 13:50:06 -0300

    validations

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um email')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No minimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
+
+export default function SignUp() {
+  function handleSubmit(data) {}
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login </Link>
+      </Form>
+    </>
+  );
+}
